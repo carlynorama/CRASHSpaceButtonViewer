43887f8763b1e2f3d1d8a76bf576b6709d1cdc7a
carlynorama <carlynorama@gmail.com>
2026-05-23 11:58:23 -0700

    displays in page

--- button_fetch.js
+++ button_fetch.js
@@ -15,6 +15,7 @@ async function getLatest(callRoute) {
         }
 
         const data = await response.json();
+        //no longer check the data here. make sure refresh() is getting it. 
         console.log(data);
         return data;
     } catch (error) {
@@ -22,8 +23,27 @@ async function getLatest(callRoute) {
     }
 }
 
+function refreshPage(){
+    getLatest(source_url).then(data => {
+        if (data) {
+            var date = new Date(data.timestamp);
+            //it was a choice to name the id's the same as the expected data labels
+            //LEARNER GUT CHECK: (data.is_open = true) shows "OPEN" can you explain why?  
+            if (data.is_open == "true") {
+                document.getElementById('is_open').innerHTML = "OPEN"
+            } else {
+                document.getElementById('is_open').innerHTML = "CLOSED"
+            }
+            
+            document.getElementById('minutes_left').innerHTML = data.minutes_left
+        }
+    });
+}
+
+
+//---------------------------------- PAGE STARTS HERE
 window.onload = function() {
     console.log('script called')
-    getLatest(source_url)
+    refreshPage()
 }
 
