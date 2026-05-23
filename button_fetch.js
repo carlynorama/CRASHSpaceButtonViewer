65a44eb314ad9914e5f40dc032c861d2ca8f1907
carlynorama <carlynorama@gmail.com>
2026-05-23 10:45:53 -0700

    fetch works

--- button_fetch.js
+++ button_fetch.js
@@ -1,6 +1,29 @@
+let callRoute = `https://crashspacela.com/sign/?output=jsonmin`
 
+async function getLatest() {
+    console.log(callRoute)
+    try {
+        const response = await fetch(
+            callRoute,
+            {
+                method: 'GET',
+            },
+        );
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 window.onload = function() {
     console.log('script called')
+    getLatest()
 }
 
